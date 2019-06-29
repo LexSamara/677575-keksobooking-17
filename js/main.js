5aeabf06b603f06efeeedfba19f8523b31ad26f0
LexSamara <eryomin.aa@gmail.com>
2019-06-29 14:16:09 +0300

    Исправлены ошибки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var moksObjectArray = [];
 var offerTypeArray = ['palace', 'flat', 'house', 'bungalo'];
 var map = document.querySelector('.map');
@@ -8,28 +10,27 @@ var map = document.querySelector('.map');
 
 // Возвращает случайное место
 var getRandomPlace = function (placesArray) {
-  return (placesArray[Math.floor(Math.random()*4)]);
+  return (placesArray[Math.floor(Math.random() * 4)]);
 };
 
 // Возвращает случайное целое в заданном диапазоне
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
 for (var i = 0; i < 8; i++) {
-moksObjectArray [i] = {
-  author: {
-    avatar: 'img/avatars/user0' + (i + 1) + '.png'
-  },
-  offer: {
-    type: getRandomPlace(offerTypeArray)
-  },
-  location: {
-    x: getRandomInt(0, 1200),
-    y: getRandomInt(130, 630)
-  }
-}
-console.log(moksObjectArray[i]);
+  moksObjectArray [i] = {
+    author: {
+      avatar: 'img/avatars/user0' + (i + 1) + '.png'
+    },
+    offer: {
+      type: getRandomPlace(offerTypeArray)
+    },
+    location: {
+      x: getRandomInt(0, 1200),
+      y: getRandomInt(130, 630)
+    }
+  };
 }
 
 map.classList.remove('map--faded');
@@ -38,21 +39,14 @@ var pinsTemplate = document.querySelector('#pin')
     .content
     .querySelector('button');
 
-console.log('Шаблон:' + pinsTemplate);
+var mapPins = document.querySelector('.map__pins');
 
-var mapPins = document.querySelector('.map__pins')
-
-for (var i = 0; i < 8; i++) {
+for (i = 0; i < 8; i++) {
   var pinElement = pinsTemplate.cloneNode(true);
   pinElement.style.left = moksObjectArray[i].location.x + 'px';
   pinElement.style.top = moksObjectArray[i].location.y + 'px';
   pinElement.querySelector('img').src = moksObjectArray[i].author.avatar;
   pinElement.alt = moksObjectArray[i].offer.type;
-  console.log('Элемент ' + i);
-  console.log('x: ' + pinElement.style.left);
-  console.log('y: ' + pinElement.style.top);
-  console.log('link: ' + pinElement.querySelector('img').src);
-  console.log('place ' + pinElement.alt);
   mapPins.appendChild(pinElement);
 }
 
