eeb883bda2a955987c991b215b2c6911eb53e5a4
LexSamara <eryomin.aa@gmail.com>
2019-07-30 19:35:44 +0300

    Исправлены замечания

--- js/card.js
+++ js/card.js
@@ -9,25 +9,39 @@
       .content
       .querySelector('.map__card');
     var cardElementArray = [];
+    var HotelTypes = {
+      'flat': 'Квартира',
+      'bungalo': 'Бунгало',
+      'house': 'Дом',
+      'palace': 'Дворец'
+    };
+
+    var renderType = function (type, el) {
+      var hotelType = HotelTypes[type];
+      el.querySelector('.popup__type').textContent = hotelType;
+    };
+
+    var renderFeatures = function (features) {
+      features.forEach(function (feature) {
+        var featureElement = document.createElement('li');
+        featureElement.classList.add('popup__feature', 'popup__feature--' + feature);
+        cardElementFeatures.appendChild(featureElement);
+      });
+    };
+
+    var renderPhoto = function (photos, el) {
+      var cardElementPhotos = el.querySelector('.popup__photos');
+      for (var j = 0; j < photos.length; j++) {
+        var imgElement = cardElementPhotos.querySelector('img').cloneNode(true);
+        imgElement.src = photos[j];
+        cardElementPhotos.appendChild(imgElement);
+      }
+      cardElementPhotos.removeChild(cardElementPhotos.querySelector('img'));
+    };
 
     for (var i = 0; i < resp.length; i++) {
       var cardElement = cardTemplate.cloneNode(true);
       var cardElementFeatures = cardElement.querySelector('.popup__features');
-
-      var cardElementWifi = cardElementFeatures.querySelector('.popup__feature--wifi');
-      var cardElementDishwasher = cardElementFeatures.querySelector('.popup__feature--dishwasher');
-      var cardElementParking = cardElementFeatures.querySelector('.popup__feature--parking');
-      var cardElementWasher = cardElementFeatures.querySelector('.popup__feature--washer');
-      var cardElementElevator = cardElementFeatures.querySelector('.popup__feature--elevator');
-      var cardElementConditioner = cardElementFeatures.querySelector('.popup__feature--conditioner');
-      var cardElementPhotos = cardElement.querySelector('.popup__photos');
-
-      var isWifi = false;
-      var isDishwasher = false;
-      var isParking = false;
-      var isWasher = false;
-      var isElevator = false;
-      var isConditioner = false;
 
       cardElement.querySelector('.popup__title').textContent = resp[i].offer.title;
       cardElement.querySelector('.popup__text--address').textContent = resp[i].offer.address;
@@ -37,69 +51,18 @@
       cardElement.querySelector('.popup__description').textContent = resp[i].offer.description;
       cardElement.querySelector('.popup__avatar').textContent = resp[i].author.avatar;
 
-      switch (resp[i].offer.type) {
-        case 'flat':
-          cardElement.querySelector('.popup__type').textContent = 'Квартира';
-          break;
-        case ('bungalo'):
-          cardElement.querySelector('.popup__type').textContent = 'Бунгало';
-          break;
-        case ('house'):
-          cardElement.querySelector('.popup__type').textContent = 'Дом';
-          break;
-        case ('palace'):
-          cardElement.querySelector('.popup__type').textContent = 'Дворец';
-          break;
+      renderType(resp[i].offer.type, cardElement);
+
+      if (resp[i].offer.features.length === 0) {
+        cardElementFeatures.classList.add('hidden');
+      } else {
+        while (cardElementFeatures.firstChild) {
+          cardElementFeatures.removeChild(cardElementFeatures.firstChild);
+        }
+        renderFeatures(resp[i].offer.features);
       }
 
-      for (var j = 0; j < resp[i].offer.features.length; j++) {
-        if (resp[i].offer.features[j] === 'wifi') {
-          isWifi = true;
-        }
-        if (resp[i].offer.features[j] === 'dishwasher') {
-          isDishwasher = true;
-        }
-        if (resp[i].offer.features[j] === 'parking') {
-          isParking = true;
-        }
-        if (resp[i].offer.features[j] === 'washer') {
-          isWasher = true;
-        }
-        if (resp[i].offer.features[j] === 'elevator') {
-          isElevator = true;
-        }
-        if (resp[i].offer.features[j] === 'conditioner') {
-          isConditioner = true;
-        }
-      }
-
-      if (!isWifi) {
-        cardElementFeatures.removeChild(cardElementWifi);
-      }
-      if (!isDishwasher) {
-        cardElementFeatures.removeChild(cardElementDishwasher);
-      }
-      if (!isParking) {
-        cardElementFeatures.removeChild(cardElementParking);
-      }
-      if (!isWasher) {
-        cardElementFeatures.removeChild(cardElementWasher);
-      }
-      if (!isElevator) {
-        cardElementFeatures.removeChild(cardElementElevator);
-      }
-      if (!isConditioner) {
-        cardElementFeatures.removeChild(cardElementConditioner);
-      }
-
-      for (j = 0; j < resp[i].offer.photos.length; j++) {
-        var imgElement = cardElementPhotos.querySelector('img').cloneNode(true);
-        imgElement.src = resp[i].offer.photos[j];
-        cardElementPhotos.appendChild(imgElement);
-      }
-
-      cardElementPhotos.removeChild(cardElementPhotos.querySelector('img'));
-
+      renderPhoto(resp[i].offer.photos, cardElement);
       cardElementArray[i] = cardElement;
     }
 
